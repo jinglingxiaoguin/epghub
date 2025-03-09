063efbe410fb4849181c848f1af3c6e7031dae27
jinglingxiaoguin <58650263+jinglingxiaoguin@users.noreply.github.com>
2025-03-09 13:15:11 +0800

    Update xmltv.py

--- epg/generator/xmltv.py
+++ epg/generator/xmltv.py
@@ -2,8 +2,13 @@
 
 from lxml import etree
 from epg.model import Channel
-from datetime import datetime
+from datetime import datetime, timezone
 
+def fix_datetime(dt):
+    if dt.tzinfo is None:
+        return dt.replace(tzinfo=timezone.utc)
+    else:
+        return dt.astimezone(timezone.utc)
 
 def write(filepath: str, channels: list[Channel], info: str = "") -> bool:
     root = etree.Element("tv")
@@ -12,7 +17,7 @@ def write(filepath: str, channels: list[Channel], info: str = "") -> bool:
     root.set("generator-info-name", info)
     last_update_time_list = []
     for channel in channels:
-        last_update_time_list.append(channel.metadata["last_update"])
+        last_update_time_list.append(fix_datetime(channel.metadata["last_update"]))
         channel_element = etree.SubElement(root, "channel")
         channel_element.set("id", channel.id)
         for name in channel.metadata["name"]:
@@ -25,19 +30,25 @@ def write(filepath: str, channels: list[Channel], info: str = "") -> bool:
             last_update_time.year,
             last_update_time.month,
             last_update_time.day,
+            last_update_time.hour,
+            last_update_time.minute,
+            last_update_time.second,
             tzinfo=last_update_time.tzinfo,
         ).strftime("%Y%m%d%H%M%S %z"),
     )
     for channel in channels:
+        for program in channel.programs:
+            program.start_time = fix_datetime(program.start_time)
+            program.end_time = fix_datetime(program.end_time)
         channel.programs.sort(key=lambda x: x.start_time)
         for program in channel.programs:
             program_element = etree.SubElement(root, "programme")
             program_element.set(
                 "start", program.start_time.astimezone().strftime("%Y%m%d%H%M%S %z")
-            )  # astimezone() is necessary
+            )
             program_element.set(
                 "stop", program.end_time.astimezone().strftime("%Y%m%d%H%M%S %z")
-            )  # astimezone() is necessary
+            )
             program_element.set("channel", channel.id)
             title = etree.SubElement(program_element, "title")
             title.text = program.title
